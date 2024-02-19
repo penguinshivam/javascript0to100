6f705fd763038f3e53da9fa24e37502cfa86e8dc
xoshivam <shivaXagarwal@gmail.com>
2024-02-19 11:37:21 +0000

    conversion od datatypes

---
+++ basic/conversion.js
@@ -0,0 +1,21 @@
+// let score = "33a"
+// console.log(typeof score)
+// let value =Number (score)
+// console.log(typeof value);
+// console.log(typeof value);
+// console.log(value);
+// // "33" => 33
+// // "33abc" => NaN
+// //  true => 1; false => 0
+
+// let is ="h"
+// let bool=Boolean(is)
+// console.log(bool);
+
+// // 1 => true; 0 => false
+// // "" => false
+// // "hitesh" => true
+let somenumber = 33
+let string = String(somenumber)
+console.log(string)
+console.log(typeof string)
