8289aa9028ddcd4a29267937625ca9af1ede05d0
xoshivam <shivaXagarwal@gmail.com>
2024-02-19 10:42:29 +0000

    datatypes

---
+++ basic/datatypes.js
@@ -0,0 +1,16 @@
+"use strict"//treat all js code as newer version
+
+//alert("hello") we are using node not in browser
+
+console.log(3+3) 
+console.log("hello");
+console.log(3+3) ;console.log("hello"); //dont use this type of editing becase of code readabily
+// ecma https://tc39.es/ecma262/ for java docs
+
+
+let name="shivam"
+let age=20
+let isloogedin=false
+
+
+// number => 2 to power 53
---
+++ basic/var.js
@@ -0,0 +1,11 @@
+const accountId = 1445
+let accountemail="ghome"
+// var accountPassword = "12345" dont use var because of less of scope 
+accountcity="jaipur"
+
+// accountId=2 not allowed because of const
+accountemail="ggg"
+accountPassword="22222" 
+accountcity="delhi"
+console.log(accountId);
+console.table([accountId,accountPassword,accountemail,accountcity])
