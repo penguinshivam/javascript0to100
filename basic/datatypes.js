5fef450bfb7249637fd8669356bf7232dde04df8
xoshivam <shivaXagarwal@gmail.com>
2024-02-19 11:16:34 +0000

    data types complete

--- basic/datatypes.js
+++ basic/datatypes.js
@@ -11,6 +11,20 @@ console.log(3+3) ;console.log("hello"); //dont use this type of editing becase o
 let name="shivam"
 let age=20
 let isloogedin=false
-
-
+let hi=null
+console.log(hi);
+console.log(typeof NaN);
 // number => 2 to power 53
+ // number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
