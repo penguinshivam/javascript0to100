eb7b33d7ee2c54936e93260c0b2f14f886f6a8c8
xoshivam <shivaXagarwal@gmail.com>
2024-02-20 18:14:44 +0000

    string variables

--- basic/conversion.js
+++ basic/conversion.js
@@ -21,6 +21,15 @@
 // console.log(typeof string)
 // *********************** Operations ***********************
 
-let value = 3
-let negValue = -value
-console.log(negValue);
+// let value = 3
+// let negValue = -value
+// console.log(negValue);
+
+let str1 ="hello"
+let str2 =" shivam"
+console.log(str1+str2);
+console.log(1+"2");
+console.log("2"+1);
+console.log("1"+"2");
+console.log("1"+2+2);
+console.log(1+2+"3");
