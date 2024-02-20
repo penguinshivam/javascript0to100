0e91b29bf5540ddef5ef90890bf96f98bf2dd582
xoshivam <shivaXagarwal@gmail.com>
2024-02-20 19:56:35 +0000

    stack and heap memory allocation

---
+++ basic/stackheap.js
@@ -0,0 +1,15 @@
+// stack (primitive)(copy) heap (non - primitive)(referance value)
+
+let myyouname="techboss"
+let anothername=myyouname
+anothername="newname"
+console.log(myyouname);
+console.log(anothername);
+ let user1 ={
+    email:"ghome@gmail.com",
+    upi:"65782346@ybl"
+ }
+let user2=user1
+user2.email="new@gmail.com"
+ console.log(user1);
+ console.log(user2);
