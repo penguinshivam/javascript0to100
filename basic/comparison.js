e7977f0b49d238bcc0dd072297ba641e0f447e59
xoshivam <shivaXagarwal@gmail.com>
2024-02-20 18:47:23 +0000

    comparision symbols

---
+++ basic/comparison.js
@@ -0,0 +1,12 @@
+console.log(2>1);
+console.log("2">1);
+console.log("hello">1);
+console.log(null>0);
+console.log(null==0);
+
+console.log(undefined==undefined);
+console.log(undefined==1);
+
+// === strict check
+
+console.log("2"=== 2);
