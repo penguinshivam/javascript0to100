e357b9ee839f000f9aa88306bdcda5aebde832e0
xoshivam <shivaXagarwal@gmail.com>
2024-02-20 18:38:23 +0000

    prefix and postfix

--- basic/conversion.js
+++ basic/conversion.js
@@ -1,29 +1,29 @@
-// let score = "33a"
-// console.log(typeof score)
-// // let value =Number (score)
-// console.log(typeof value);
-// console.log(typeof value);
-// console.log(value);
-// // "33" => 33
-// // "33abc" => NaN
-// //  true => 1; false => 0
+let score = "33a"
+console.log(typeof score)
+let alue =Number (score)
+console.log(typeof value);
+console.log(typeof value);
+console.log(value);
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
 
-// let is ="h"
-// let bool=Boolean(is)
-// console.log(bool);
+let is ="h"
+let bool=Boolean(is)
+console.log(bool);
 
-// // 1 => true; 0 => false
-// // "" => false
-// // "hitesh" => true
-// let somenumber = 33
-// let string = String(somenumber)
-// console.log(string)
-// console.log(typeof string)
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+let somenumber = 33
+let string = String(somenumber)
+console.log(string)
+console.log(typeof string)
 // *********************** Operations ***********************
 
-// let value = 3
-// let negValue = -value
-// console.log(negValue);
+let value = 3
+let negValue = -value
+console.log(negValue);
 
 let str1 ="hello"
 let str2 =" shivam"
@@ -33,3 +33,25 @@ console.log("2"+1);
 console.log("1"+"2");
 console.log("1"+2+2);
 console.log(1+2+"3");
+
+console.log(2+false);
+console.log(+"");
+
+let num1,num2,num3
+num1=num2=num3=2+2
+
+let gamecounter=100
+gamecounter++;
+++gamecounter;
+console.log(gamecounter);
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
