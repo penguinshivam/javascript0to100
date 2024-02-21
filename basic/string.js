7625c587c6107d805b2267b0841074b5eed6351c
xoshivam <shivaXagarwal@gmail.com>
2024-02-21 18:35:52 +0000

    string function basic

---
+++ basic/string.js
@@ -0,0 +1,18 @@
+const name ="shivam"
+const repocount=50
+console.log(`hello my name is ${name} my repocount is ${repocount}`);
+const gamename =new String ('shivam-game')
+console.log(gamename[1]);
+console.log(gamename.length);
+console.log(gamename.toUpperCase());
+console.log(gamename.charAt(4));
+console.log(gamename.indexOf("a"));
+console.log(gamename.substring(0,4));
+const newstring="   shivam   "
+console.log(newstring);
+console.log(newstring.trimStart());
+const url ="   www.googlec.com"
+console.log(url);
+console.log(url.replace("c","").trim());
+console.log(url.includes("google"));
+console.log(gamename.split("-"));
