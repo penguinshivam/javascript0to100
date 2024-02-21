ce44f84dd9b0b1f38c0c2fe69aa6f4099b476125
xoshivam <shivaXagarwal@gmail.com>
2024-02-21 19:09:37 +0000

    maths and nums

---
+++ basic/nums_and_maths.js
@@ -0,0 +1,39 @@
+// const score = 400
+// console.log(score);
+
+// const balance =new Number(400)
+// console.log(balance);
+// console.log(balance.toString());
+// console.log(score.toString().length);
+
+// console.log(score.length);
+// console.log(balance.toFixed(3));
+// console.log(score.toFixed(3));
+// const othernumber=1234.46
+// console.log(othernumber.toPrecision(4));
+
+// const hund=100000000
+// console.log(hund.toLocaleString());
+// console.log(hund.toLocaleString('en-IN'));
+// console.log(Number.MAX_SAFE_INTEGER);
+// console.log(Number);
+
+
+
+// maths
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.6));
+// console.log(Math.floor(4.6));
+
+// console.log(Math.min(4,3,5,6));
+
+console.log(Math.random());
+console.log((Math.random()*10 )+1);
+
+const min =1
+const max = 4
+
+console.log(Math.floor(Math.random()*(max -min +1))+min)
