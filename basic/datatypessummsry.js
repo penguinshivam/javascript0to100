10189d29c1f122a0aeaf7f6688969197b61a51ea
xoshivam <shivaXagarwal@gmail.com>
2024-02-21 19:13:24 +0000

    data types summary

--- basic/datatypessummsry.js
+++ basic/datatypessummsry.js
@@ -45,3 +45,42 @@ console.log(typeof null)
 //        Arrays  =>  object
 //        Function  =>  function
 //        Object  =>  object
+
+
+//  Primitive
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score1 = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id1 = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 3456543576654356754n
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heos = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
--- basic/nums_and_maths.js
+++ basic/nums_and_maths.js
@@ -37,3 +37,7 @@ const min =1
 const max = 4
 
 console.log(Math.floor(Math.random()*(max -min +1))+min)
+
+
+
+
