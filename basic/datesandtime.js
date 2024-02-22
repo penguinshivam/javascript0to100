5abcd9b2d81dc89fd1dabb70077da97e0567bf14
xoshivam <shivaXagarwal@gmail.com>
2024-02-22 19:47:26 +0000

    basic of dates

---
+++ basic/datesandtime.js
@@ -0,0 +1,22 @@
+// let mydate = new Date()
+// console.log(mydate.toString());
+// console.log(mydate.toDateString());
+// console.log(mydate.toISOString());
+// console.log(mydate.toJSON());
+// console.log(mydate.toLocaleDateString());
+// console.log(typeof mydate);
+
+let mycreteddate=new Date(2023,1,23)
+console.log(mycreteddate.toDateString());
+// months start from monday
+let mycreteddateandtime=new Date(2023,1,23,5,4)
+console.log(mycreteddateandtime.toLocaleString());
+
+let mycreteddateformate=new Date("01-06-2024")
+console.log(mycreteddateformate.toDateString());
+// mm dd yyyy
+
+let mytimestamp= Date.now()
+console.log(mytimestamp);
+
+console.log(Math.floor(Date.now()/1000));
