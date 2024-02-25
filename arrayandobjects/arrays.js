c5ff89da989e98610e01e0bded5d22ecd466fc93
xoshivam <shivaXagarwal@gmail.com>
2024-02-25 18:43:54 +0000

    array completed

--- arrayandobjects/arrays.js
+++ arrayandobjects/arrays.js
@@ -1,74 +1,77 @@
-const myarray=[0,1,2,3,4,5,6]
-console.log(myarray[1]);
-console.log(myarray.length);
+// const myarray=[0,1,2,3,4,5,6]
+// console.log(myarray[1]);
+// console.log(myarray.length);
 
 
-myarray.push(6)
-myarray.push(7)
-console.log(myarray);
-myarray.pop()
-console.log(myarray);
-myarray.unshift(8)
-console.log(myarray);
-myarray.shift(8)
-console.log(myarray);
-console.log(myarray.includes(4));//datatype is boolean
-const newArr = myarray.join()
+// myarray.push(6)
+// myarray.push(7)
+// console.log(myarray);
+// myarray.pop()
+// console.log(myarray);
+// myarray.unshift(8)
+// console.log(myarray);
+// myarray.shift(8)
+// console.log(myarray);
+// console.log(myarray.includes(4));//datatype is boolean
+// const newArr = myarray.join()
 
 
-console.log( newArr);
-console.log( typeof newArr);
-
-// slice, splice
-
-console.log("A ", myarray);
-
-const my1 = myarray.slice(1, 3)
-
-console.log(myn1);
-console.log("B ", myarray);
-
-
-const my2 = myarray.splice(1, 3)//splice modify the orignal array and remove the part we slected 
-console.log("C ", myarray);
-console.log(myn2);
-
-// array
-
-const myArr = [0, 1, 2, 3, 4, 5]
-const myHeors = ["shaktiman", "naagraj"]
-
-const myArr2 = new Array(1, 2, 3, 4)
-// console.log(myArr[1]);
-
-// Array methods
-
-// myArr.push(6)
-// myArr.push(7)
-// myArr.pop()
-
-// myArr.unshift(9)
-// myArr.shift()
-
-// console.log(myArr.includes(9));
-// console.log(myArr.indexOf(3));
-
-// const newArr = myArr.join()
-
-// console.log(myArr);
 // console.log( newArr);
+// console.log( typeof newArr);
+
+// // slice, splice
+
+// console.log("A ", myarray);
+
+// const my1 = myarray.slice(1, 3)
+
+// console.log(myn1);
+// console.log("B ", myarray);
 
 
-// slice, splice
+// const my2 = myarray.splice(1, 3)//splice modify the orignal array and remove the part we slected 
+// console.log("C ", myarray);
+// console.log(myn2);
 
-console.log("A ", myArr);
+// // array
 
-const myn1 = myArr.slice(1, 3)
+// const myArr = [0, 1, 2, 3, 4, 5]
+// const myHeors = ["shaktiman", "naagraj"]
 
-console.log(myn1);
-console.log("B ", myArr);
+// const myArr2 = new Array(1, 2, 3, 4)
+// // console.log(myArr[1]);
+
+// // Array methods
+
+// // myArr.push(6)
+// // myArr.push(7)
+// // myArr.pop()
+
+// // myArr.unshift(9)
+// // myArr.shift()
+
+// // console.log(myArr.includes(9));
+// // console.log(myArr.indexOf(3));
+
+// // const newArr = myArr.join()
+
+// // console.log(myArr);
+// // console.log( newArr);
 
 
-const myn2 = myArr.splice(1, 3)
-console.log("C ", myArr);
-console.log(myn2);
+// // slice, splice
+
+// console.log("A ", myArr);
+
+// const myn1 = myArr.slice(1, 3)
+
+// console.log(myn1);
+// console.log("B ", myArr);
+
+
+// const myn2 = myArr.splice(1, 3)
+// console.log("C ", myArr);
+// console.log(myn2);
+
+
+
