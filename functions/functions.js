21be734f825f1535100d63ecdbd1a198987a7f9f
xoshivam <shivaXagarwal@gmail.com>
2024-02-26 20:14:33 +0000

    function completed

--- functions/functions.js
+++ functions/functions.js
@@ -33,3 +33,33 @@ function loginusermessage(username="shivam"){
 // console.log(loginusermessage("shivam"));
 console.log(loginusermessage("hello"));
 
+function calculatecartprice(...num1){//... rest and spread operater
+    return num1
+
+}
+
+console.log(calculatecartprice(200,400,500)); 
+
+const user = {
+    username: "hitesh",
+    prices: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
